4434d67a9776e471364db6315fccce431f5dbda4
ayalos <reginaldoayalos0@gmail.com>
2018-12-15 13:38:59 +0000

    icons

--- SRA/src/controller/Tela_AlunoController.java
+++ SRA/src/controller/Tela_AlunoController.java
@@ -2,13 +2,41 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sra.Tela_Aluno_Cadastro;
 
 public class Tela_AlunoController implements Initializable {
 
+    @FXML private Button btn_adicionar_aluno;
+    @FXML private Button btn_deletar_aluno;
+    @FXML private Button btn_editar_aluno;
+    @FXML private Button btn_atualizar_table_view;
+    @FXML private TextField label_texto_pesquisa;
+    @FXML private Button btn_pesquisar;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        btn_adicionar_aluno.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                Tela_Aluno_Cadastro tela_Aluno_Cadastro = new Tela_Aluno_Cadastro(); // ESTANCIANDO OBJETO DA TELA PRINCIPAL
+                try {
+                    tela_Aluno_Cadastro.start(new Stage()); // INICIANDO A TELA PRINCIPAL ( PASSANDO O STAGE COM ARGUMENTO )
+                } catch (Exception ex) { // TRATAMENTO DE ERRO ( EXCEPRION )
+                    Logger.getLogger(TelaLoginController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
     }    
     
 }
---
+++ SRA/src/sra/Tela_Aluno_Cadastro.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sra;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author klebson
+ */
+public class Tela_Aluno_Cadastro extends Application {
+    
+    private static Stage stage; //Janela 
+    
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Tela_Aluno_Cadastro.fxml")); // CARREGANDO FXML
+        
+        Scene scene = new Scene(root); // COLOCANDO FXML NA CENA 
+        stage.setTitle("Aluno Cadastro - SRA"); // COLOCANDO UM TITULO 
+        stage.setScene(scene); /// PASSANDO A CENA PARA UMA JANELA 
+        stage.setResizable(false); // NAO É POSSIVEL REDIMENSIONAR A JANELA
+        
+        
+        stage.show(); // ABRINDO A JANELA
+        
+        setStage(stage);
+    }
+
+    public static Stage getStage() {
+        return stage;
+    }
+
+    public static void setStage(Stage stage) {
+        Tela_Aluno_Cadastro.stage = stage;
+    }
+}
